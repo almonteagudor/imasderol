e7005798db170d1a22c244cbb7fe7d2b20f4086e
almonteagudor <55769333+almonteagudor@users.noreply.github.com>
2024-01-12 18:21:42 +0100

    Packages added

---
+++ imasderol.infrastructure/ImasderolDbContext.cs
@@ -0,0 +1,14 @@
+﻿using imasderol.domain.zombicide.skill;
+using Microsoft.EntityFrameworkCore;
+
+namespace imasderol.infrastructure;
+
+public class ImasderolDbContext : DbContext
+{
+    public DbSet<Skill>? Skills { get; set; }
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=imasderol.db");
+    }
+}
---
+++ imasderol.infrastructure/zombicide/skill/SkillRepository.cs
@@ -0,0 +1,43 @@
+﻿using imasderol.domain.shared.exceptions;
+using imasderol.domain.zombicide.skill;
+using Microsoft.EntityFrameworkCore;
+
+namespace imasderol.infrastructure.zombicide.skill;
+
+public class SkillRepository(ImasderolDbContext context) : ISkillRepository
+{
+    public Skill FindById(Guid id)
+    {
+        var skill = context.Skills!.FirstOrDefault(s => s.Id == id);
+
+        if (skill is null)
+        {
+            throw new NotFoundException($"Skill not found | {id}");
+        }
+
+        return skill;
+    }
+
+    public IEnumerable<Skill> FindAll()
+    {
+        return context.Skills!.ToList();
+    }
+
+    public void Save(Skill skill)
+    {
+        context.Skills!.Add(skill);
+        context.SaveChanges();
+    }
+
+    public void Update(Skill skill)
+    {
+        context.Entry(skill).State = EntityState.Modified;
+        context.SaveChanges();
+    }
+
+    public void Delete(Skill skill)
+    {
+        context.Entry(skill).State = EntityState.Deleted;
+        context.SaveChanges();
+    }
+}
