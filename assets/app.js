f1a441ca7c026432b9a0a5786e7bc93d795b0a81
almonteagudor <55769333+almonteagudor@users.noreply.github.com>
2024-06-14 15:39:09 +0200

    Package "what-input" added to importmap.php

--- assets/app.js
+++ assets/app.js
@@ -2,4 +2,5 @@ import './styles/app.css';
 import 'foundation-sites/dist/css/foundation.min.css';
 
 import './bootstrap.js';
+import 'what-input';
 import 'foundation-sites/dist/js/foundation.min.js';
