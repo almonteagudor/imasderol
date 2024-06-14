f45c6518f796f816acaf52ec06ddd7bc710f0459
almonteagudor <55769333+almonteagudor@users.noreply.github.com>
2024-06-14 15:02:46 +0200

    foundation framework imports added

--- assets/app.js
+++ assets/app.js
@@ -1,9 +1,4 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * This file will be included onto the page via the importmap() Twig function,
- * which should already be in your base.html.twig.
- */
 import './styles/app.css';
+import 'foundation-sites/dist/css/foundation.min.css';
 
-console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+import 'foundation-sites/dist/js/foundation.min.js';
