439126ef98fd7bff7562526d531ee1b8ccb71a05
almonteagudor <55769333+almonteagudor@users.noreply.github.com>
2024-07-07 13:21:54 +0200

    symfony 7.1

--- assets/app.js
+++
@@ -1,6 +0,0 @@
-import './styles/app.css';
-import 'foundation-sites/dist/css/foundation.min.css';
-
-import './bootstrap.js';
-import 'what-input';
-import 'foundation-sites/dist/js/foundation.min.js';
